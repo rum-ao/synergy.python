7dfd6203bebf9f9f9de1f613650c275658158924
r.a.o <145882811+rum-ao@users.noreply.github.com>
2024-04-03 10:17:22 +1100

    Create 3-7.py

---
+++ 3-7.py
@@ -0,0 +1,10 @@
+stroka1 = input()
+if stroka1[::-1] == stroka1:
+    print("yes")
+else:
+    print("no")
+    
+stroka2 = input()
+while "  " in stroka2:
+    stroka2 = stroka2.replace("  ", " ")
+print(stroka2)

c762a7fb31ba598f1048b3b60ea2fe6168e484d6
r.a.o <145882811+rum-ao@users.noreply.github.com>
2024-04-02 11:56:29 +1100

    Add files via upload

---
+++ _2_3.py
@@ -0,0 +1,13 @@
+type = input("vvedite vid zhivotnogo: ")
+age = int(input("vvedite vozrast zhivotnogo: "))
+nick = input("vvedite klichku zhivotnogo: ")
+print(f'Eto {type} po klichke "{nick}". Vozrast: {age} goda.')
+
+stage1 = input("vvedite 1 etap razvitiya cheloveka: ")
+stage2 = input("vvedite 2 etap razvitiya cheloveka: ")
+stage3 = input("vvedite 3 etap razvitiya cheloveka: ")
+stage4 = input("vvedite 4 etap razvitiya cheloveka: ")
+stage5 = input("vvedite 5 etap razvitiya cheloveka: ")
+stage6 = input("vvedite 6 etap razvitiya cheloveka: ")
+stage7 = input("vvedite 7 etap razvitiya cheloveka: ")
+print (stage1, stage2, stage3, stage4, stage5, stage6, stage7, sep='=>')

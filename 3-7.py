0bc97000afd68b3ff9a77b2b0b93523d2d0ef1a9
r.a.o <145882811+rum-ao@users.noreply.github.com>
2024-04-03 11:36:41 +1100

    Update 3-7.py

--- 3-7.py
+++ 3-7.py
@@ -3,7 +3,7 @@ if stroka1[::-1] == stroka1:
     print("yes")
 else:
     print("no")
-    
+#######################################
 stroka2 = input()
 while "  " in stroka2:
     stroka2 = stroka2.replace("  ", " ")
